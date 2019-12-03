74add27a567eac6c328e7505ebdab409eb857ed4
Andtii <andy@ekdahls.net>
2019-12-04 00:40:02 +0100

    Update to .net core 3.1

--- src/Omnia.ProcessManagement.Web/Startup.cs
+++ src/Omnia.ProcessManagement.Web/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Omnia.ProcessManagement.Core.Extensions;
 using Swashbuckle.AspNetCore.Swagger;
@@ -29,7 +30,7 @@ namespace Omnia.ProcessManagement.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
