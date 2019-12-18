f71311d45c8b43eb30e55eca89c689608acde1bd
Duong Tran Lap <lapduongbkit@gmail.com>
2019-12-18 16:23:02 +0700

    Update localization

--- src/Omnia.ProcessManagement.Web/client/components/permissiondialog/loc/localize.manifest.ts
+++ src/Omnia.ProcessManagement.Web/client/components/permissiondialog/loc/localize.manifest.ts
@@ -5,7 +5,7 @@ Composer.registerManifest("B6461912-81C6-4F66-9FFC-83CFD0D42B46")
     .registerLocalization()
     .namespace(OPMPermissionDialogLocalization.namespace)
     .add<OPMPermissionDialogLocalization.locInterface>({
-        DialogTitle: "Controlled Documents Library Permissions",
+        DialogTitle: "Process Library Permissions",
         NoPermissionMsg: "You don't have permission",
         Authors: "Authors",
         DefaultReaders: "Default Readers"
