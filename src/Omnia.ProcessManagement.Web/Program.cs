09879a75b0377c1ae2e4ff2d3a7f6efb535ec487
PetterT2 <petter.theander@preciofishbone.se>
2019-12-19 17:33:51 +0100

    Fix30 startup config (#21)
    * added omnia nuget version
    
    * added omnia nugetversion
    Changed startup config to  match 3.0

--- src/Omnia.ProcessManagement.Web/Program.cs
+++ src/Omnia.ProcessManagement.Web/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Omnia.Fx.HostConfiguration.Extensions;
 using Omnia.Fx.NetCore.WebApp.Hosting;
 using Omnia.ProcessManagement.Core.EnterpriseProperties;
 using Omnia.ProcessManagement.Core.Extensions;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Omnia.ProcessManagement.Web.Security.ResourceEvaluators;
 
 namespace Omnia.ProcessManagement.Web
 {
@@ -12,27 +17,75 @@ namespace Omnia.ProcessManagement.Web
 
         public static async Task Main(string[] args)
         {
-            await BuildWebHost(args)
-                    .RunOmniaAsync();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
-        public static IWebHost BuildWebHost(string[] args)
+        /// <summary>
+        /// Build host here to support add migration
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            return new WebAppHost(args)
-                .ConfigureOmnia<Startup>((omniaConfig, logging) =>
-                {
+            return new OmniaHostBuilder(args)
+                    .ConfigureOmniaFx((omniaConfig, logger) =>
+                    {
                     omniaConfig
+                        .AddAppSettingsJsonFile("appsettings.json")
                         .AddAppSettingsJsonFile("appsettings.local.json")
                         .AddOmniaFxWebApp((options)=> {
-                            options.AddOmniaMvc();
                             options.DefaultConfiguration();
                             options.AddEnterprisePropertyFieldProvider(
                                    EnterprisePropertyFieldProviderOptionsHelper.ConfigureEnterprisePropertyFieldProvider);
                         })
                         .AddOmniaFxNetCoreSharePoint()
                         .AddOmniaPMCore(); 
-                })
-                .Build();
+                }).ConfigureHost(hostbuilder =>
+                        hostbuilder
+                        .ConfigureServices(services => {
+                            services.AddRouting();
+
+                            services.AddOmniaPMSqlDB();
+
+                            services.AddScopedWithSingeltonRef<ISecurityResourceIdResourceEvaluator, SecurityResourceIdResourceEvaluator>();
+                            services.AddScopedWithSingeltonRef<IOPMProcessIdResourceEvaluator, OPMProcessIdResourceEvaluator>();
+
+                            // Register the Swagger generator, defining one or more Swagger documents
+                            services.AddSwaggerGen(c =>
+                            {
+                                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                            });
+                        })
+                        .ConfigureWebHost(webHostBuilder =>
+                        {
+                            webHostBuilder.Configure((ctx, app) =>
+                            {
+                                app.UseStaticFiles();
+
+                                app.UseRouting();
+
+                                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                                app.UseSwagger();
+
+                                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+                                app.UseSwaggerUI(c =>
+                                {
+                                    c.RoutePrefix = "swagger";
+                                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Omnia.Workplace.Web V1");
+                                });
+
+                                //Middleware
+
+                                app.UseAuthentication();
+                                app.UseAuthorization();
+
+                                app.UseEndpoints(endpoints => {
+                                    endpoints.MapControllers();
+                                });
+                            });
+                        })
+                    );
+
         }
     }
 }
