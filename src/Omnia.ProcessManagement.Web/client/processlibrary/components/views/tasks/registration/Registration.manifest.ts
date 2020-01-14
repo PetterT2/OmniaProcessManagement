064176f0f4878c586cc7edba2c51a524f368ab1b
Duong Tran Lap <lapduongbkit@gmail.com>
2020-01-14 09:25:12 +0700

    Task processor

---
+++ src/Omnia.ProcessManagement.Web/client/processlibrary/components/views/tasks/registration/Registration.manifest.ts
@@ -0,0 +1,13 @@
+﻿import { Composer } from "@omnia/tooling-composers";
+import { Guid } from '@omnia/fx/models';
+import { ResourceManifests, ServiceDefinition } from '@omnia/workplace/models';
+
+Composer
+    .registerManifest(new Guid("FECE2831-389D-4FAA-BA13-FC5795707D46"), "opm.processlibrary.taskprocessor")
+    .registerResources({
+        resourcePaths: ['./Registration.js']
+    })
+    .withLoadRules().loadIfManifestLoaded({
+        omniaServiceId: ServiceDefinition.Id.toString(),
+        resourceId: ResourceManifests.MyTasksCore.toString()
+    })
