fffead4dd31a4bf74340537d9c34e960f3f7cab1
Cuong Nguyen Duong Thanh <Cuong.Nguyen.Duong.Thanh@preciofishbone.se>
2020-02-14 14:51:33 +0700

    Refactor data model #50

---
+++ src/Omnia.ProcessManagement.Core/Migrations/20200214074939_AddShapeTemplateTable.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Omnia.ProcessManagement.Core.Migrations
+{
+    public partial class AddShapeTemplateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShapeTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    ModifiedAt = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(nullable: true),
+                    ClusteredId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    BuiltIn = table.Column<bool>(nullable: false),
+                    JsonValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShapeTemplates", x => x.Id)
+                        .Annotation("SqlServer:Clustered", false);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShapeTemplateImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShapeTemplateId = table.Column<Guid>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    ModifiedBy = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(nullable: false),
+                    ModifiedAt = table.Column<DateTimeOffset>(nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(nullable: true),
+                    FileName = table.Column<string>(nullable: true),
+                    Content = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShapeTemplateImages", x => new { x.Id, x.ShapeTemplateId });
+                    table.ForeignKey(
+                        name: "FK_ShapeTemplateImages_ShapeTemplates_ShapeTemplateId",
+                        column: x => x.ShapeTemplateId,
+                        principalTable: "ShapeTemplates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShapeTemplateImages_ShapeTemplateId",
+                table: "ShapeTemplateImages",
+                column: "ShapeTemplateId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShapeTemplates_ClusteredId",
+                table: "ShapeTemplates",
+                column: "ClusteredId",
+                unique: true)
+                .Annotation("SqlServer:Clustered", true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShapeTemplateImages");
+
+            migrationBuilder.DropTable(
+                name: "ShapeTemplates");
+        }
+    }
+}
